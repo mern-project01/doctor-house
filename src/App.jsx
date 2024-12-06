146e526508bc040115baee509e058d0786b7e572
mern-project01 <mernp29@gmail.com>
2024-12-06 22:44:40 +0600

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod commodi doloremque soluta ullam distinctio molestias, quo culpa odit repellendus nisi voluptas praesentium modi officia sint exercitationem earum natus expedita iusto!</p>
+      <P>
+        i want add some more information or add a new topic in git
+      </P>
+   </div>
+  )
+}
+
+export default App
